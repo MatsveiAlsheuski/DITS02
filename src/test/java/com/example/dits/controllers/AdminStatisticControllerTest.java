230d8aeb8f700246ac258faa9b3a94498fa2a09c
Matsvei Alsheuski <matsvei.alsheuski@gmail.com>
2022-06-14 21:43:01 +0300

    Create Test for AdminHomeControllerTest

--- src/test/java/com/example/dits/controllers/AdminStatisticControllerTest.java
+++ src/test/java/com/example/dits/controllers/AdminStatisticControllerTest.java
@@ -1,24 +1,13 @@
 package com.example.dits.controllers;
 
-import com.example.dits.DAO.StatisticRepository;
-import com.example.dits.entity.Statistic;
-import com.example.dits.entity.Topic;
-import com.example.dits.service.TopicService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,8 +18,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AdminStatisticControllerTest {
     @Autowired
     private MockMvc mockMvc;
-    @MockBean
-    private TopicService topicService;
 
     @Test
     public void testTestStatisticOnAdmin() throws Exception {
